3ce551834488c170b5fe03febfd6e435aed26521
Ahmed <061221@hdi.de>
2019-05-29 18:32:15 +0200

    Added the checking of inscribed ellipse part

--- IoU scripts/inscribed_circle.py
+++ IoU scripts/inscribed_circle.py
@@ -20,6 +20,30 @@ def display_image(image):
 
     return
 
+def check_point(point_x, point_y, center_x, center_y, minor_axis, major_axis):
+    result = (( (point_x-center_x)**2 ) / major_axis**2) + ( ( (point_y - center_y)**2 ) / minor_axis**2)
+    
+    return True if result < 1 else False
+
+def return_ellipse_mask(image, origin_point_y, origin_point_x, major_axis, minor_axis):
+    pixels = image.shape
+
+    width, height = pixels[1], pixels[0]
+    
+    mask_array = np.zeros((width, height), dtype=bool)
+    
+    for i in range(width):
+        for j in range(height):
+            mask_array[i,j] = check_point(i, j, origin_point_y, origin_point_x, minor_axis, major_axis)
+            
+            if mask_array[i,j]:
+                image[i,j] = np.array([0,0,255])
+
+
+    return mask_array, image
+
+###############################################################################
+    
 img = np.zeros((512,512,3), np.uint8)
 
 
@@ -43,4 +67,11 @@ img = cv2.rectangle(img, top_left_coordinate, bottom_right_coordinate, color, th
 
 ellipse_circle_img = cv2.ellipse(img, (origin_point_x, origin_point_y), (major_axis, minor_axis), 0,0,360, COLOR_BLUE, 1)
 
-display_image(ellipse_circle_img)
+###############################################################################
+
+mask_array , image = return_ellipse_mask(ellipse_circle_img, origin_point_y, origin_point_x, minor_axis, major_axis)
+
+
+###############################################################################
+
+display_image(image)
